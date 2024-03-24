dfae555bc4194a57da167604e72b0ca1ec720cb8
Charles Arulraj <charlesarulraj@Charless-MacBook-Air.local>
2024-03-24 00:55:52 -0700

    first commit

---
+++ Sorting.java
@@ -0,0 +1,37 @@
+package Ecom.Newcom;
+
+public class Sorting {
+	static void max_array(int arr[]) {
+		int max = 0;
+		for (int i = 0; i < arr.length; i++) {
+			max = arr[0];
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+
+		}
+		System.out.print(max);
+
+	}
+
+	static void min_array(int arr[]) {
+		int min = 0;
+		min=arr[0];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]<min)
+			{
+				min=arr[i];
+			}
+		}
+		System.out.print(min);
+
+	}
+
+	public static void main(String args[]) {
+		int array[] = { 1, 2, 4, 6, 3, 7 };
+		Sorting.max_array(array);
+		Sorting.min_array(array);
+	}
+	
+}
